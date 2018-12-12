d64ee04d183ca6c834829e72da90c2feb9c5c46a
thyrissara <36397255+thyrissara@users.noreply.github.com>
2018-12-12 12:36:32 +0700

    Added confiner

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float smoothTime;
+
+    private Vector3 targetPosition;
+    private Vector3 currentVelocity;
+
+    public void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RunBackward();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            RunForward();
+        }
+        UpdateToRealPosition();
+    }
+
+    private void UpdateToRealPosition()
+    {
+       transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
+    }
+
+    public void RunForward()
+    {
+        targetPosition += new Vector3(speed, 0, 0);
+    }
+
+    private void RunBackward()
+    {
+        targetPosition -= new Vector3(speed, 0, 0);
+    }
+}
