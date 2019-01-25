17dd898b50210198bdb1e9794b18822ebbab0da0
thyrissara <36397255+thyrissara@users.noreply.github.com>
2019-01-25 19:01:48 +0700

    added stories

---
+++ Assets/StoryCanvas.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryCanvas : MonoBehaviour
+{
+    public StoryText[] storySequence;
+    public string goToSceneAfterTheEnd;
+
+    private int currentPage = 0;
+
+    void Start()
+    {
+        RunStory();
+    }
+
+    public void RunStory()
+    {
+        ShowStoryAtPage(0);
+        currentPage = 0;
+    }
+
+    public void AdvanceStory()
+    {
+        currentPage++;
+        if(currentPage >= storySequence.Length)
+        {
+            SceneManager.LoadScene(goToSceneAfterTheEnd);
+        }
+        else
+        {
+            ShowStoryAtPage(currentPage);
+        }
+    }
+
+    private void ShowStoryAtPage(int page)
+    {
+        StoryText selectedPage = storySequence[page];
+        //...
+    }
+}
---
+++ Assets/StoryText.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StoryText : ScriptableObject
+{
+    public string who;
+    [Multiline]
+    public string text;
+    public Sprite backgroundImage;
+}
