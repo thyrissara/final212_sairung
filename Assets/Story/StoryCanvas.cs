a1e6df74e768b99a68d891169eae7e0e38eb9625
thyrissara <36397255+thyrissara@users.noreply.github.com>
2019-02-03 16:26:19 +0700

    Added story sequence with fade

--- Assets/Story/StoryCanvas.cs
+++ Assets/Story/StoryCanvas.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StoryCanvas : MonoBehaviour
 {
     public StoryText[] storySequence;
     public string goToSceneAfterTheEnd;
+    public PlayableDirector inDirector;
+    public PlayableDirector outDirector;
+
+    public TextMeshProUGUI narrationText;
+    public TextMeshProUGUI whoText;
+    public Image bgImage;
+    public CanvasGroup whoCanvasGroup;
+
+    public GraphicRaycaster graphicRaycaster;
 
     private int currentPage = 0;
 
@@ -36,7 +48,37 @@ public class StoryCanvas : MonoBehaviour
 
     private void ShowStoryAtPage(int page)
     {
+        StartCoroutine(ChangeStoryRoutine(page));
+    }
+
+    IEnumerator ChangeStoryRoutine(int page)
+    {
+        graphicRaycaster.enabled = false;
+        //Fade out
+        if (page != 0)
+        {
+            outDirector.Play();
+            yield return new WaitForSeconds((float)outDirector.playableAsset.duration);
+        }
+
+        //Change
         StoryText selectedPage = storySequence[page];
-        //...
+
+        narrationText.text = selectedPage.text;
+        if (selectedPage.who == "")
+        {
+            whoCanvasGroup.alpha = 0;
+        }
+        else
+        {
+            whoCanvasGroup.alpha = 1;
+            whoText.text = selectedPage.who;
+        }
+        bgImage.sprite = selectedPage.backgroundImage;
+
+        //Fade in
+        inDirector.Play();
+        yield return new WaitForSeconds((float)inDirector.playableAsset.duration);
+        graphicRaycaster.enabled = true;
     }
 }
